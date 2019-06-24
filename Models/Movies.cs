c1000b9f9e995fe4ae5d42b920b7a6c1715feb15
bermanjacob96 <bermanjacob96@gmail.com>
2019-06-24 13:55:30 -0600

    Adding Validation

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -7,15 +7,25 @@ namespace RazorPagesMovie.Models
         public class Movie
         {
             public int ID { get; set; } //REQ by Database for primary key
+
+            [StringLength(60, MinimumLength = 3)]
+            [Required]
             public string Title { get; set; }
             
-            [Display(Name = "Release Date")]
-            [DataType(DataType.Date)]
+            [Display(Name = "Release Date"), DataType(DataType.Date)]
+            //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
             public DateTime ReleaseDate { get; set; }
+
+            [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), StringLength(30)]
+            [Required]
             public string Genre { get; set; }
 
+            [Range(1,100), DataType(DataType.Currency)]
             [Column(TypeName = "decimal(18,2)")]
             public decimal Price { get; set; }
+
+            [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+            [Required]
             public string Rating { get; set; }
         }
 }
